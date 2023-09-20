255136b4b7409577c5fb2d0bf9f25fa7073c7743
GOPI THAMMISETTI <gopithammisetti6@gmail.com>
2023-09-20 10:29:16 +0530

    Add Linked List

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+         LL list=new LL();
+         list.insertFirst(3);
+         list.insertFirst(4);
+         list.insertFirst(5);
+         list.insertFirst(23);
+         list.insertFirst(12);
+        list.insertLast(1);
+        list.insertLast(212);
+        list.insert(100,2);
+        System.out.println(list.deleteFirst());
+         list.display();
+        System.out.println(list.deleteLast());
+        list.display();
+        System.out.println(list.delete(2));
+        list.display();
+
+    }
+}
